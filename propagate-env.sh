090fed33134a0020f86d8e7876024c8621a01c50
Heecheon Park <heecheon@thesesamelab.com>
2026-04-06 11:43:30 +0900

    env propagation script added

---
+++ propagate-env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in {1..5}; do
+  cp module-$i/studio/.env.example module-$i/studio/.env
+  echo "OPENAI_API_KEY=\"$OPENAI_API_KEY\"" > module-$i/studio/.env
+done
+echo "TAVILY_API_KEY=\"$TAVILY_API_KEY\"" >> module-4/studio/.env
